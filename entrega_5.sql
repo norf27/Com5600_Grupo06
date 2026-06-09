b4592e38935527c0ab5f06f7f7e7293f8f946496
Thiago Luna gauna <lunagaunathiago@gmail.com>
2026-06-08 23:40:47 -0300

    Update entrega_5.sql
    tablas de guardaparques y guias (como estan en el der, pendiente añadir mas atributos)

--- entrega_5.sql
+++ entrega_5.sql
@@ -42,7 +42,7 @@ CUIL varchar(13) not null,
 constraint FK_parque foreign key (ID_parque) references Parque(ID),
 constraint check_DNI check (DNI like '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
 constraint check_sueldo check (sueldo > 0),
-constraint check_nacimiento check (nacimiento <= dateadd(year, -18, cast(getdate() as date))), --que sea >= 18 años
+constraint check_nacimiento check (nacimiento <= dateadd(year, -18, cast(getdate() as date))), --que sea >= 18 aÃ±os
 constraint check_CUIL check (CUIL like '[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9]')
 )
 --CONCESIONES
@@ -85,3 +85,66 @@ ID_concesion bigint not null,
 constraint FK_concesion foreign key (ID_concesion) references Concesion(ID),
 constraint check_monto check (monto > 0)
 )
+
+--GUARDAPARQUES Y GUIAS
+CREATE TABLE Guardaparque (
+    ID_Empleado BIGINT PRIMARY KEY,
+    CONSTRAINT FK_Guardaparque_Empleado FOREIGN KEY (ID_Empleado) REFERENCES Empleado(ID) ON DELETE CASCADE
+);
+CREATE TABLE Guardaparque_Parque (
+    ID_Guardaparque BIGINT,
+    ID_Parque INT, 
+    Fecha_ingreso DATE NOT NULL,
+    Fecha_egreso DATE NULL,
+    Motivo_egreso VARCHAR(255) NULL,
+    
+    PRIMARY KEY (ID_Guardaparque, ID_Parque, Fecha_ingreso),
+    CONSTRAINT FK_GuardaparqueParque_Guardaparque FOREIGN KEY (ID_Guardaparque) REFERENCES Guardaparque(ID_Empleado),
+    CONSTRAINT CK_GuardaparqueParque_Fechas CHECK (Fecha_egreso IS NULL OR Fecha_egreso >= Fecha_ingreso),
+    CONSTRAINT CK_GuardaparqueParque_ConsistenciaEgreso CHECK (
+        (Fecha_egreso IS NULL AND Motivo_egreso IS NULL) OR 
+        (Fecha_egreso IS NOT NULL AND Motivo_egreso IS NOT NULL)
+    )
+);
+CREATE TABLE Guia (
+    ID_Empleado BIGINT PRIMARY KEY,
+    CONSTRAINT FK_Guia_Empleado FOREIGN KEY (ID_Empleado) REFERENCES Empleado(ID) ON DELETE CASCADE
+);
+CREATE TABLE Habilitacion (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Detalles VARCHAR(100) NOT NULL,
+    Fecha DATE NOT NULL
+);
+CREATE TABLE Guia_Habilitacion (
+    ID_Guia BIGINT,
+    ID_Habilitacion INT,
+    PRIMARY KEY (ID_Guia, ID_Habilitacion),
+    CONSTRAINT FK_GuiaHabilitacion_Guia FOREIGN KEY (ID_Guia) REFERENCES Guia(ID_Empleado),
+    CONSTRAINT FK_GuiaHabilitacion_Habilitacion FOREIGN KEY (ID_Habilitacion) REFERENCES Habilitacion(ID)
+);
+CREATE TABLE Especialidad (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL
+);
+CREATE TABLE Guia_Especialidad (
+    ID_Guia BIGINT,
+    ID_Especialidad INT,
+    PRIMARY KEY (ID_Guia, ID_Especialidad),
+    CONSTRAINT FK_GuiaEspecialidad_Guia FOREIGN KEY (ID_Guia) REFERENCES Guia(ID_Empleado),
+    CONSTRAINT FK_GuiaEspecialidad_Especialidad FOREIGN KEY (ID_Especialidad) REFERENCES Especialidad(ID)
+);
+
+CREATE TABLE Titulo (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Fecha DATE NOT NULL,
+    Origen VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Guia_Titulo (
+    ID_Guia BIGINT,
+    ID_Titulo INT,
+    PRIMARY KEY (ID_Guia, ID_Titulo),
+    CONSTRAINT FK_GuiaTitulo_Guia FOREIGN KEY (ID_Guia) REFERENCES Guia(ID_Empleado),
+    CONSTRAINT FK_GuiaTitulo_Titulo FOREIGN KEY (ID_Titulo) REFERENCES Titulo(ID)
+);
