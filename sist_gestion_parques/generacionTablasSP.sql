57323d12d7846970f00eebe07a268105d080468e
Santiago Grasso <santidgr2006@gmail.com>
2026-06-15 20:01:40 -0300

    modificacion para archivos (incompleta, falta modificar cosas de una tabla)

--- sist_gestion_parques/generacionTablasSP.sql
+++ sist_gestion_parques/generacionTablasSP.sql
@@ -58,6 +58,57 @@ BEGIN
 	EXEC('CREATE SCHEMA Atracciones'); 
 END
 GO
+
+IF SCHEMA_ID('Staging') IS NULL
+BEGIN
+	EXEC('CREATE SCHEMA Staging'); 
+END
+GO
+
+------------------ CREACIÓN DE TABLAS DE STAGING -------------------
+
+-- Archivo Nacional (aprn_f_defensa_2026.csv)
+IF OBJECT_ID('Staging.STG_DefensaAreasProtegidas', 'U') IS NULL
+BEGIN
+    CREATE TABLE Staging.STG_DefensaAreasProtegidas (
+        nombre VARCHAR(MAX) NULL,
+        provincia VARCHAR(MAX) NULL,
+        creacion VARCHAR(MAX) NULL,
+        hectareas VARCHAR(MAX) NULL,
+        ambiente_protegido VARCHAR(MAX) NULL
+    );
+END
+GO
+
+-- Archivo Internacional (WDPA_WDOECM_Jun2026_Public_ARG_csv.csv)
+IF OBJECT_ID('Staging.STG_WDPA_Areas', 'U') IS NULL
+BEGIN
+    CREATE TABLE Staging.STG_WDPA_Areas (
+        TYPE VARCHAR(MAX) NULL,
+        SITE_ID VARCHAR(MAX) NULL,
+        NAME VARCHAR(MAX) NULL,
+        DESIG VARCHAR(MAX) NULL,
+        STATUS VARCHAR(MAX) NULL,
+        STATUS_YR VARCHAR(MAX) NULL,
+        GIS_AREA VARCHAR(MAX) NULL,
+        MANG_AUTH VARCHAR(MAX) NULL
+    );
+END
+GO
+
+-- Tabla de Auditoria de Errores
+IF OBJECT_ID('Staging.Log_Errores_Importacion', 'U') IS NULL
+BEGIN
+    CREATE TABLE Staging.Log_Errores_Importacion (
+        ID_Log INT IDENTITY(1,1) PRIMARY KEY,
+        Fecha_Proceso DATETIME DEFAULT GETDATE(),
+        Archivo_Origen VARCHAR(255) NOT NULL,
+        Fila_Contenido_Raw VARCHAR(MAX) NOT NULL,
+        Motivo_Error VARCHAR(500) NOT NULL
+    );
+END
+GO
+	
 ------------------ CREACIÓN DE TABLAS -------------------
 --------------------PARQUE-----------------------
 IF OBJECT_ID('Parque.Tipo_parque', 'U') IS NULL
