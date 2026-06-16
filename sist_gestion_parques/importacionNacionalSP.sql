12f28f74bce1abd569cf03b5b99d0bdccaef2a31
Santiago Grasso <santidgr2006@gmail.com>
2026-06-16 00:46:04 -0300

    correccion temporal (dsp lo termino)

--- sist_gestion_parques/importacionNacionalSP.sql
+++ sist_gestion_parques/importacionNacionalSP.sql
@@ -48,28 +48,29 @@ BEGIN
 
     UPDATE P
     SET 
-        P.Provincia = TRIM(S.provincia),
+        P.Superficie = Staging.FN_TransformarAreaAHectareas(S.hectareas, 'HA'),
         P.Anio_Creacion = TRY_CAST(S.creacion AS INT),
-        P.Hectareas = Staging.FN_TransformarAreaAHectareas(S.hectareas, 'HA'),
         P.Ambiente_Ecoregion = TRIM(S.ambiente_protegido),
+        P.ID_provincia = (SELECT TOP 1 ID FROM Parque.Provincia WHERE UPPER(TRIM(Nombre)) = UPPER(TRIM(S.provincia))),
         P.Fecha_Ultima_Actualizacion = GETDATE()
-    FROM Parque.ParqueNacional P
+    FROM Parque.Parque P
     INNER JOIN Staging.STG_DefensaAreasProtegidas S 
-        ON UPPER(TRIM(P.Nombre)) = UPPER(TRIM(S.nombre)) 
-    WHERE TRY_CAST(S.hectareas AS DECIMAL(12,2)) IS NOT NULL;
+        ON UPPER(TRIM(P.Nombre)) = UPPER(TRIM(S.nombre))
+    WHERE TRY_CAST(S.hectareas AS DECIMAL(12,2)) IS NOT NULL; 
 
-    INSERT INTO Parque.ParqueNacional (Nombre, Provincia, Anio_Creacion, Hectareas, Ambiente_Ecoregion)
+    INSERT INTO Parque.Parque (Nombre, Superficie, ID_tipo, ID_provincia, Anio_Creacion, Ambiente_Ecoregion)
     SELECT 
         TRIM(S.nombre), 
-        TRIM(S.provincia), 
+        Staging.FN_TransformarAreaAHectareas(S.hectareas, 'HA'),
+        NULL, 
+        (SELECT TOP 1 ID FROM Parque.Provincia WHERE UPPER(TRIM(Nombre)) = UPPER(TRIM(S.provincia))),
         TRY_CAST(S.creacion AS INT), 
-        Staging.FN_TransformarAreaAHectareas(S.hectareas, 'HA'), 
         TRIM(S.ambiente_protegido)
     FROM Staging.STG_DefensaAreasProtegidas S
     WHERE TRY_CAST(S.hectareas AS DECIMAL(12,2)) IS NOT NULL
       AND NOT EXISTS (
           SELECT 1 
-          FROM Parque.ParqueNacional P 
+          FROM Parque.Parque P 
           WHERE UPPER(TRIM(P.Nombre)) = UPPER(TRIM(S.nombre))
       );
 END;
