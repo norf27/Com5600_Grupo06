0432c38e88fe045397792b5d8e61d4f57f7e35e2
Thiago Luna gauna <lunagaunathiago@gmail.com>
2026-06-18 16:15:41 -0300

    Update reporteDeudores.sql

--- sist_gestion_parques/reporteDeudores.sql
+++ sist_gestion_parques/reporteDeudores.sql
@@ -21,7 +21,7 @@ inner join Concesiones.Empresa e on c.ID_empresa = e.ID
 inner join Parque.Parque p on p.ID = c.ID_parque
 on c.ID = pm.ID_concesion
 where c.Estado = 'A' and  pm.Estado = 'A' and pm.Pago = 'D' and pm.Fecha < @Fecha
-for xml path('Deudor'), root('Deudores');
+FOR XML PATH('Deudor'), ROOT('Reportes');
 
 
 
