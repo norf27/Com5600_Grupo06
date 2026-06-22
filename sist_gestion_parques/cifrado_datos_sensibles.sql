fb391893d603f7dab8c72f84c181f14a40621c47
Federico Cuda <124699038+fedeC363@users.noreply.github.com>
2026-06-22 17:28:53 -0300

    Add encrypted columns and update sensitive data
    Added columns for encrypted sensitive data in Empleados and Ventas tables and implemented encryption for sensitive fields.

--- sist_gestion_parques/cifrado_datos_sensibles.sql
+++ sist_gestion_parques/cifrado_datos_sensibles.sql
@@ -1,1 +1,96 @@
+USE sist_gestion_parques;
+GO
+
+------------------ COLUMNAS CIFRADAS: EMPLEADO -------------------
+
+IF COL_LENGTH('Empleados.Empleado', 'DNI_CifradoFraseClave') IS NULL
+BEGIN
+	ALTER TABLE Empleados.Empleado
+	ADD DNI_CifradoFraseClave VARBINARY(256) NULL;
+END
+GO
+
+IF COL_LENGTH('Empleados.Empleado', 'Sueldo_CifradoFraseClave') IS NULL
+BEGIN
+	ALTER TABLE Empleados.Empleado
+	ADD Sueldo_CifradoFraseClave VARBINARY(256) NULL;
+END
+GO
+
+IF COL_LENGTH('Empleados.Empleado', 'CUIL_CifradoFraseClave') IS NULL
+BEGIN
+	ALTER TABLE Empleados.Empleado
+	ADD CUIL_CifradoFraseClave VARBINARY(256) NULL;
+END
+GO
+
+------------------ COLUMNAS CIFRADAS: CLIENTE -------------------
+
+IF COL_LENGTH('Ventas.Cliente', 'Tipo_doc_CifradoFraseClave') IS NULL
+BEGIN
+	ALTER TABLE Ventas.Cliente
+	ADD Tipo_doc_CifradoFraseClave VARBINARY(256) NULL;
+END
+GO
+
+IF COL_LENGTH('Ventas.Cliente', 'Documento_CifradoFraseClave') IS NULL
+BEGIN
+	ALTER TABLE Ventas.Cliente
+	ADD Documento_CifradoFraseClave VARBINARY(256) NULL;
+END
+GO
+
+------------------ CIFRADO DE DATOS SENSIBLES -------------------
+
+-- Obtenemos la clave de cifrado. Lo cargariamos desde otra capa.
+DECLARE @FraseClaveCargadaPorUsuario NVARCHAR(128);
+SET @FraseClaveCargadaPorUsuario = 'MessiGoat';
+
+-- Ciframos los datos sensibles de empleados.
+-- Se agrega un hash/autenticador usando la PK ID de Empleados.Empleado.
+UPDATE Empleados.Empleado
+SET DNI_CifradoFraseClave =
+		EncryptByPassPhrase(
+			@FraseClaveCargadaPorUsuario,
+			CONVERT(VARBINARY(MAX), DNI),
+			1,
+			CONVERT(VARBINARY, ID)
+		),
+	Sueldo_CifradoFraseClave =
+		EncryptByPassPhrase(
+			@FraseClaveCargadaPorUsuario,
+			CONVERT(VARBINARY(MAX), CONVERT(VARCHAR(32), Sueldo)),
+			1,
+			CONVERT(VARBINARY, ID)
+		),
+	CUIL_CifradoFraseClave =
+		EncryptByPassPhrase(
+			@FraseClaveCargadaPorUsuario,
+			CONVERT(VARBINARY(MAX), CUIL),
+			1,
+			CONVERT(VARBINARY, ID)
+		);
+GO
+
+-- Ciframos los datos sensibles de clientes.
+-- Se agrega un hash/autenticador usando la PK ID de Ventas.Cliente.
+DECLARE @FraseClaveCargadaPorUsuario NVARCHAR(128);
+SET @FraseClaveCargadaPorUsuario = 'MessiGoat';
+
+UPDATE Ventas.Cliente
+SET Tipo_doc_CifradoFraseClave =
+		EncryptByPassPhrase(
+			@FraseClaveCargadaPorUsuario,
+			CONVERT(VARBINARY(MAX), Tipo_doc),
+			1,
+			CONVERT(VARBINARY, ID)
+		),
+	Documento_CifradoFraseClave =
+		EncryptByPassPhrase(
+			@FraseClaveCargadaPorUsuario,
+			CONVERT(VARBINARY(MAX), Documento),
+			1,
+			CONVERT(VARBINARY, ID)
+		);
+GO
 
