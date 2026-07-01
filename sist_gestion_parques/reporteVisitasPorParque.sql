220eb44b0a12601ebee4bb7d8376a2b2c96833c5
Federico Cuda <124699038+fedeC363@users.noreply.github.com>
2026-07-01 00:05:13 -0300

    Update reporteVisitasPorParque.sql

--- sist_gestion_parques/reporteVisitasPorParque.sql
+++ sist_gestion_parques/reporteVisitasPorParque.sql
@@ -1,6 +1,7 @@
 /*
 Fecha: 3/07/2026
-Integrantes: Cuda Federico, Santiago Grasso, Luna Gauna Thiago Gonzalo, Nicolas Orfano
+Universidad Nacional de La Matanza, Bases de Datos Aplicadas
+Integrantes: Cuda Federico, Grasso Santiago, Luna Gauna Thiago Gonzalo, Orfano Nicolas 
 Descripcion: Script para tabla cruzada mostrando visitas por mes y parque.
 */
 
